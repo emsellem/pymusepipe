45163b52ba868394bc436cf9d421fc840a537149
Eric Emsellem <eric.emsellem@eso.org>
2018-04-06 17:45:28 +0200

    Version 0.6.0 with bias working

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(name='pymusepipe',
-      version='0.5.0',
+      version='0.6.0',
       description='python module to reduce MUSE LP data associated with PHANGS',
       long_description=readme,
       keywords='MUSE, PHANGS',
