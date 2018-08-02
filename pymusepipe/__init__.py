2fd108c4a14094fdbaddf4e03ef08f255bd69f25
Eric Emsellem <eric.emsellem@eso.org>
2018-08-02 10:41:24 +0200

    Version 0.9.9.8 - Refined alignment tool

--- pymusepipe/__init__.py
+++ pymusepipe/__init__.py
@@ -4,7 +4,7 @@
     This package is a wrapper around MUSE pipeline commands to reduce
     muse dataframes.
 """
-from pymusepipe import (init_musepipe, musepipe, graph_pipe, mpdaf_pipe,
+from pymusepipe import (init_musepipe, musepipe, target_sample, graph_pipe, mpdaf_pipe,
         check_pipe, emission_lines, prep_recipes_pipe, recipes_pipe, create_sof, util_pipe,
         align_pipe)
 
--- pymusepipe/init_musepipe.py
+++ pymusepipe/init_musepipe.py
@@ -13,7 +13,6 @@ import os
 from os.path import join as joinpath
 import copy
 from pymusepipe import util_pipe as upipe
-from pymusepipe.musepipe import MusePipe
 
 ############################################################
 #                      BEGIN
@@ -51,24 +50,6 @@ dic_calib_tables = {
             # Filter List
             "filter_list" : "filter_list.fits",
             }
-
-# ----------------- Galaxies and Pointings ----------------#
-
-# Sample of galaxies
-# For each galaxy, we provide the pointings numbers and the run attached to that pointing
-MUSEPIPE_sample = {
-        "NGC628": {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0},
-        "NGC1087": {1:1}, 
-        "NGC1672": {1:1, 2:1, 3:1, 4:1, 5:1}
-        }
-
-# List of MUSEPIPE Observing runs
-# For each Observing run, we provide the start and end dates
-MUSEPIPE_runs = {
-        'Run00' : ['P099','2017-10-01','2017-10-31'],
-        'Run01' : ['P100','2017-10-01','2017-10-31'], 
-        'Run02' : ['P101','2017-11-01','2017-11-30']
-        }
 
 ############################################################
 #                      END
@@ -215,77 +196,3 @@ class InitMuseParameters(object) :
                    "We will use the default hardcoded value from "
                    "init_musepipe.py").format(param=key))
             setattr(self, key, dic_param[key])
-
-
-####################################################
-# Defining classes to get samples and objects
-####################################################
-class MusepipeSample(object) :
-    def __init__(self) :
-        self.sample = MUSEPIPE_sample
-        self.targets = MUSEPIPE_sample.keys()
-
-class MusepipeTarget(object) :
-    def __init__(self, galaxyname=None, list_pointings=[1]) :
-        if galaxyname not in MUSEPIPE_sample.keys() :
-            upipe.print_error("ERROR: no Galaxy named {gal} in the defined sample".format(gal=galaxyname))
-            return
-
-        # Galaxy name
-        upipe.print_info("Initialising Target {name}".format(name=galaxyname))
-        self.targetname = galaxyname
-
-        # Info of the pointings and extracting the observing run for each pointing
-        self.info_pointings = MUSEPIPE_sample[galaxyname]
-        if any([_ not in self.info_pointings.keys() for _ in list_pointings]) :
-            upipe.print_error("ERROR: no pointing {0} for the Galaxy".format(list_pointings))
-            return
-        self.list_pointings = list_pointings
-        self.observing_run = [self.info_pointing[_] for _ in self.list_pointings]
-
-    def _get_file_name(self, suffix, pointing):
-        return "{0}_P{1:02d}.txt".format(suffix, pointing)
-
-    def run_pipeline(self, list_pointings=[1], fakemode=False, 
-            suffix_logfile="logfile", suffix_rcfile="rcfile", suffix_calfile="calfile"):
-        """Run the pipeline for all pointings in the list
-        """
-        if any([_ not in self.info_pointings.keys() for _ in self.list_pointings]) :
-            upipe.print_error("ERROR: some pointing are not in "
-                "the available list ({0})".format(self.list_pointings))
-            return
-
-        # Setting up the suffixes for the files
-        self.suffix_logfile = suffix_logfile
-        self.suffix_calfile = suffix_calfile
-        self.suffix_rcfile = suffix_rcfile
-
-        # Loop on the pointings
-        self.pipelines = []
-        upipe.print_info("---- Starting the Data Reduction ----")
-        self.history = []
-        for pointing in list_pointings:
-
-            # Setting up the names of the output files
-            logfile = self._get_logfile_name(self.suffix_logfile, pointing)
-            calfile = self._get_calfile_name(self.suffix_calfile, pointing)
-            rcfile = self._get_rcfile_name(self.suffix_rcfile, pointing)
-            
-            python_command = ("mypipe = musepipe.MusePipe(galaxyname={0}, "
-                    "pointing={1}, rc_filename={2}, cal_filename={3}, "
-                    "outlog=None, logfile={4}, fakemode={5}, "
-                    "nocache=False)".format(galaxyname, pointing, rcfile, calfile, 
-                            logfile, fakemode))
-            upipe.print_info("====== START - POINTING {0:2d} ======".format(pointing))
-            upipe.print_info(python_command)
-            upipe.print_info("====== END   - POINTING {0:2d} ======".format(pointing))
-            self.history.append(python_command)
-            mypipe = MusePipe(galaxyname=galaxyname, pointing=pointing, 
-                    rc_filename=rcfile, cal_filename=calfile, outlog=None, 
-                    logfile=logfile, fakemode=fakemode, nocache=False)
-
-            self.pipelines.append(mypipe)
-            mypipe.run_all_recipes()
-
-    def combine(self):
-        pass
