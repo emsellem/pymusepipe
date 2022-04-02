722a497d6114a9cbc8dc3a26cb57606599c6cb29
Eric Emsellem <eric.emsellem@eso.org>
2022-04-02 14:39:11 +0200

    Bug fix in target_sample modifying a iterator while iterating

--- src/pymusepipe/target_sample.py
+++ src/pymusepipe/target_sample.py
@@ -784,7 +784,7 @@ class MusePipeSample(object):
         # Get the parameters for the recipes
         param_recipes = kwargs.pop("param_recipes", {})
         kwargs_recipe = {}
-        list_keys = copy.copy(kwargs.keys())
+        list_keys = list(kwargs.keys())
         for kw in list_keys:
             if kw in dict_default_for_recipes.keys():
                 kwargs_recipe[kw] = kwargs.pop(kw, dict_default_for_recipes[kw])
