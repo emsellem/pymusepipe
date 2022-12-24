2267139b2af943de418dc6b93f548da116ad8503
Eric Emsellem <eric.emsellem@eso.org>
2022-12-24 16:06:24 +0100

    Added a few print in mpdaf

--- src/pymusepipe/util_pipe.py
+++ src/pymusepipe/util_pipe.py
@@ -502,7 +502,7 @@ def filter_list_with_pdict(input_list, list_datasets=None,
             # or found it, then record it
             else:
                 # Record only if the input list
-                if fdataset in list_datasets or list_datasets is None:
+                if fdataset in list_datasets or len(list_datasets)==0:
                     if fdataset not in dict_files:
                         dict_files_with_tpl[fdataset] = {ftpl: [fnexpo]}
                     else:
